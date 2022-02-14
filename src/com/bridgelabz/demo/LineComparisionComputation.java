36da7c7d8dc40d9edc85d9bbaed5a0433a3b8af8
karthikbenki <karthikmc007@gmail.com>
2022-02-14 17:41:19 +0530

    Refactored: to oops logic

--- src/com/bridgelabz/demo/LineComparisionComputation.java
+++ src/com/bridgelabz/demo/LineComparisionComputation.java
@@ -22,18 +22,19 @@ public class LineComparisionComputation {
 	System.out.println("Welcome to Line\r\n" + "Comparison Computation\r\n" + "Program on Master Branch");
 	Points points_1 = new Points(1, 2, 5, 8);
 	Points points_2 = new Points(1, 2, 3, 4);
-	int line1Length = lineLengthCal(points_1.x1, points_1.x2, points_1.y1, points_1.y2);
-	int line2Length = lineLengthCal(points_2.x1, points_2.x2, points_2.y1, points_2.y2);
-	compareTo(line1Length, line2Length);
+	LineComparisionComputation lineComp = new LineComparisionComputation();
+	int line1Length = lineComp.lineLengthCal(points_1.x1, points_1.x2, points_1.y1, points_1.y2);
+	int line2Length = lineComp.lineLengthCal(points_2.x1, points_2.x2, points_2.y1, points_2.y2);
+	lineComp.compareTo(line1Length, line2Length);
 
     }
 
-    public static int lineLengthCal(int x1, int y1, int x2, int y2) {
+    public int lineLengthCal(int x1, int y1, int x2, int y2) {
 	int lineLength = (int) Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 	return lineLength;
     }
 
-    public static void equalityCheck(int line1Length, int line2Length) {
+    public void equalityCheck(int line1Length, int line2Length) {
 	if (line1Length == line2Length) {
 	    System.out.println("Two lines are equal");
 	} else {
@@ -41,7 +42,7 @@ public class LineComparisionComputation {
 	}
     }
 
-    public static void compareTo(int line1Length, int line2Length) {
+    public void compareTo(int line1Length, int line2Length) {
 	if (line1Length == line2Length) {
 	    System.out.println("Line 1 is equal to Line 2");
 	} else if (line1Length > line2Length) {
